fc23c1d0e025238a08dbd12e908f642accd6499b
Jose Eduardo Serrano <joser_isi@hotmail.com>
2023-03-13 19:35:11 -0600

    add exercises 13-03-2023

---
+++ excercise11/Program.cs
@@ -0,0 +1,31 @@
+﻿var resultado = Suma(6,7);
+
+
+Print(resultado);
+
+Print(Resta(5,6));
+
+
+
+int Suma(int x, int y)
+{
+    return x + y; 
+}
+
+
+int Resta (int x, int y)
+{
+    return x - y; 
+}
+
+int Multiplica (int x, int y)
+{
+    return x * y ;
+}
+
+
+void Print (int resultado)
+{
+    Console.WriteLine("Resultado = {0}", resultado);
+}
+
---
+++ excersise12/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Cuantos numeros deseas sumar?");
+
+int sizeArray = Int32.Parse(Console.ReadLine());
+int[] numeros = new int[sizeArray];
+
+
+int i = 0;
+
+while(i<numeros.Length)
+{
+    Console.WriteLine("Introduce numero {0}", i);
+    numeros[i] = Int32.Parse(  Console.ReadLine());
+    i++;
+}
+
+Console.WriteLine("El resultado es {0}", SumaArreglo(numeros)); 
+
+ 
+
+ int SumaArreglo (int [] arreglo)
+ {
+    int result = 0;
+    for(int i = 0; i < arreglo.Length; i++)
+    {   
+        result += arreglo[i];
+    }
+    return result;
+ }
